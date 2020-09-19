9f4ec6f18b54d609050baae74b752d7a10f81086
Jaymes <jaymesbunce@gmail.com>
2020-09-19 17:35:51 -0600

    Fixes

--- js/noteApp.js
+++ js/noteApp.js
@@ -2,16 +2,27 @@ function editNote(){
     let noteLoadName = document.getElementById("noteName").value;
     let noteHTML = localStorage.getItem(noteLoadName);
     document.getElementById("noteDetails").value = noteHTML;
+    
+    if (noteHTML == null){
+        window.alert("Unable to find Note with this note name.");
+    }
+    else {
+        window.alert("Note Loaded");
+    }
 }
 
 function saveNote(){
     let noteSaveName = document.getElementById("noteName").value;
     let noteHTML = document.getElementById("noteDetails").value;
-    localStorage.setItem(noteSaveName, noteHTML);
+    localStorage.setItem(noteSaveName, noteHTML);  
+    window.alert("Note Saved");
+    location.reload();
 }
 
 function clearNotes(){
     localStorage.clear();
+    window.alert("All Notes Cleared");
+    location.reload();
 }
 
 let noteNames = Object.entries(localStorage);

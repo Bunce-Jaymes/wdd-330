ab3dc239c26623e03d7874b656a817b72e32fd0c
Jaymes <jaymesbunce@gmail.com>
2020-10-21 19:26:27 -0600

    Week 6 Progress

--- js/todo/todo.js
+++ js/todo/todo.js
@@ -1,43 +1,155 @@
 let todoListItemsArray = JSON.parse(localStorage.getItem("toDoList"));
+let filiterValue = "";
 
-console.log(todoListItemsArray);
-
-function showList() {
+function showList(filiterValue) {
     const listOfTasks = document.querySelector('#listOfItemsToDo');
 
+    //deletes everything in list 
     let child = listOfTasks.lastElementChild;
-
     while (child) {
         listOfTasks.removeChild(child);
         child = listOfTasks.lastElementChild;
     };
 
-    if (todoListItemsArray !== null) {
-        for (let i = 0; i < todoListItemsArray.length; i++) {
-            const listItem = document.createElement("li");
-            const listItemInput = document.createElement("input");
-            const listItemLabel = document.createElement("label");
-            const listItemButton = document.createElement("button");
+    if (filiterValue === "all") {
+        if (todoListItemsArray !== null) {
+            for (let i = 0; i < todoListItemsArray.length; i++) {
+                const listItem = document.createElement("li");
+                const listItemInput = document.createElement("input");
+                const listItemID = document.createElement("data");
+                const listItemLabel = document.createElement("label");
+                const listItemButton = document.createElement("button");
+                //Add class to set formating of list item
+                listItem.classList.add('Item');
+                //checkbox
+                listItemInput.setAttribute('type', 'checkbox');
+                listItemInput.addEventListener('change', checkComplete);
+                //delete button
+                listItemButton.classList.add('todoButton');
+                listItemButton.addEventListener('click', deleteItem);
+                listItemButton.innerHTML = "X";
+                //id
+                listItemID.setAttribute('value', todoListItemsArray[i].id);
+                listItemID.classList.add('hidden');
+                //todo item text
+                listItemLabel.innerHTML = todoListItemsArray[i].content;
 
-            listItemInput.setAttribute('type', 'checkbox')
-            listItem.classList.add('Item');
-            listItemButton.classList.add('todoButton');
+                if (todoListItemsArray[i].completed === true) {
+                    listItemLabel.classList.add('taskComplete');
+                    listItem.classList.add('taskComplete');
+                    listItemInput.checked = true;
+                };
 
+                //Adding them all together
+                listItem.appendChild(listItemInput);
+                listItem.appendChild(listItemLabel);
+                listItem.appendChild(listItemButton);
+                listItem.appendChild(listItemID);
+                listOfTasks.appendChild(listItem);
+            }
 
-            listItemLabel.innerHTML = todoListItemsArray[i].content;
-            listItemButton.innerHTML = "X";
-
-            listItem.appendChild(listItemInput);
-            listItem.appendChild(listItemLabel);
-            listItem.appendChild(listItemButton);
-
-            listOfTasks.appendChild(listItem);
+            document.querySelector('#allTasksButton').classList.add('currentFiliter');
+            document.querySelector('#allCurrentButton').classList.remove('currentFiliter');
+            document.querySelector('#allCompletedButton').classList.remove('currentFiliter');
         }
+    };
 
-        totalTasks();
-        saveList();
-        document.querySelector('#allTasksButton').classList.add('currentFiliter');
-    }
+    if (filiterValue === "current") {
+        if (todoListItemsArray !== null) {
+            for (let i = 0; i < todoListItemsArray.length; i++) {
+
+                if (todoListItemsArray[i].completed === false) {
+                    const listItem = document.createElement("li");
+                    const listItemInput = document.createElement("input");
+                    const listItemID = document.createElement("data");
+                    const listItemLabel = document.createElement("label");
+                    const listItemButton = document.createElement("button");
+                    //Add class to set formating of list item
+                    listItem.classList.add('Item');
+                    //checkbox
+                    listItemInput.setAttribute('type', 'checkbox');
+                    listItemInput.addEventListener('change', checkComplete);
+                    //delete button
+                    listItemButton.classList.add('todoButton');
+                    listItemButton.addEventListener('click', deleteItem);
+                    listItemButton.innerHTML = "X";
+                    //id
+                    listItemID.setAttribute('value', todoListItemsArray[i].id);
+                    listItemID.classList.add('hidden');
+                    //todo item text
+                    listItemLabel.innerHTML = todoListItemsArray[i].content;
+
+                    if (todoListItemsArray[i].completed === true) {
+                        listItemLabel.classList.add('taskComplete');
+                        listItem.classList.add('taskComplete');
+                        listItemInput.checked = true;
+                    };
+
+                    //Adding them all together
+                    listItem.appendChild(listItemInput);
+                    listItem.appendChild(listItemLabel);
+                    listItem.appendChild(listItemButton);
+                    listItem.appendChild(listItemID);
+                    listOfTasks.appendChild(listItem);
+                }
+            }
+
+            document.querySelector('#allCurrentButton').classList.add('currentFiliter');
+            document.querySelector('#allTasksButton').classList.remove('currentFiliter');
+            document.querySelector('#allCompletedButton').classList.remove('currentFiliter');
+        }
+    };
+    
+    
+    if (filiterValue === "completed") {
+        if (todoListItemsArray !== null) {
+            for (let i = 0; i < todoListItemsArray.length; i++) {
+
+                if (todoListItemsArray[i].completed === true) {
+                    const listItem = document.createElement("li");
+                    const listItemInput = document.createElement("input");
+                    const listItemID = document.createElement("data");
+                    const listItemLabel = document.createElement("label");
+                    const listItemButton = document.createElement("button");
+                    //Add class to set formating of list item
+                    listItem.classList.add('Item');
+                    //checkbox
+                    listItemInput.setAttribute('type', 'checkbox');
+                    listItemInput.addEventListener('change', checkComplete);
+                    //delete button
+                    listItemButton.classList.add('todoButton');
+                    listItemButton.addEventListener('click', deleteItem);
+                    listItemButton.innerHTML = "X";
+                    //id
+                    listItemID.setAttribute('value', todoListItemsArray[i].id);
+                    listItemID.classList.add('hidden');
+                    //todo item text
+                    listItemLabel.innerHTML = todoListItemsArray[i].content;
+
+                    if (todoListItemsArray[i].completed === true) {
+                        listItemLabel.classList.add('taskComplete');
+                        listItem.classList.add('taskComplete');
+                        listItemInput.checked = true;
+                    };
+
+                    //Adding them all together
+                    listItem.appendChild(listItemInput);
+                    listItem.appendChild(listItemLabel);
+                    listItem.appendChild(listItemButton);
+                    listItem.appendChild(listItemID);
+                    listOfTasks.appendChild(listItem);
+                }
+            }
+
+            document.querySelector('#allCompletedButton').classList.add('currentFiliter');
+            document.querySelector('#allTasksButton').classList.remove('currentFiliter');
+            document.querySelector('#allCurrentButton').classList.remove('currentFiliter');
+        }
+    };
+    
+    
+    totalTasks();
+    saveList();
 };
 
 function addItemToList() {
@@ -45,7 +157,7 @@ function addItemToList() {
 
     if (todoListItemsArray !== null) {
         todoListItemsArray.push({
-            id: Date.now(),
+            id: String(Date.now()),
             content: newItemText,
             completed: false
         });
@@ -53,7 +165,7 @@ function addItemToList() {
     if (todoListItemsArray === null) {
         todoListItemsArray = [];
         todoListItemsArray.push({
-            id: Date.now(),
+            id: String(Date.now()),
             content: newItemText,
             completed: false
         });
@@ -61,20 +173,117 @@ function addItemToList() {
 
     document.querySelector('#newItemInput').value = '';
 
-    showList();
+    showList(filiterValue);
 };
 
 function totalTasks() {
-    const totalTaskNumber = document.querySelector('#tasksLeft');
-    const lengthOfList = todoListItemsArray.length;
-    totalTaskNumber.innerHTML = `${lengthOfList} tasks total`;
-}
+    const ulList = document.querySelector('#listOfItemsToDo');
+    const count = document.querySelector('#tasksLeft');
+    
+    const lengthOfList = ulList.childElementCount;
+
+    if (lengthOfList === null) {
+        count.innerHTML = `0 tasks total`;
+    } else {
+        count.innerHTML = `${lengthOfList} tasks total`;
+    }
+};
 
 function saveList() {
     localStorage.setItem("toDoList", JSON.stringify(todoListItemsArray));
+};
+
+function deleteItem() {
+    const buttonClicked = event.currentTarget;
+
+    let idToBeDeleted = buttonClicked.parentElement.lastElementChild;
+    idToBeDeleted = String(idToBeDeleted.value)
+
+    let removeIndex = todoListItemsArray.map(function (item) {
+        return item.id;
+    }).indexOf(idToBeDeleted);
+
+    todoListItemsArray.splice(removeIndex, 1);
+
+    saveList();
+    showList(filiterValue);
+};
+
+function checkComplete() {
+    const checkboxClicked = event.currentTarget;
+
+    if (checkboxClicked.checked) {
+        const itemToBeCrossed = checkboxClicked.parentElement.children.item(1);
+        itemToBeCrossed.classList.add("taskComplete");
+        checkboxClicked.parentElement.classList.add("taskComplete");
+
+        let idToBeCompleted = checkboxClicked.parentElement.lastElementChild;
+        idToBeCompleted = String(idToBeCompleted.value)
+
+        let completeIndex = todoListItemsArray.map(function (item) {
+            return item.id;
+        }).indexOf(idToBeCompleted);
+
+        todoListItemsArray[completeIndex].completed = true;
+
+        saveList();
+        showList(filiterValue);
+
+    } else {
+        const itemToBeCrossed = checkboxClicked.parentElement.children.item(1);
+        itemToBeCrossed.classList.remove("taskComplete");
+        checkboxClicked.parentElement.classList.remove("taskComplete");
+
+        let idToBeCompleted = checkboxClicked.parentElement.lastElementChild;
+        idToBeCompleted = String(idToBeCompleted.value)
+
+        let completeIndex = todoListItemsArray.map(function (item) {
+            return item.id;
+        }).indexOf(idToBeCompleted);
+
+        todoListItemsArray[completeIndex].completed = false;
+
+        saveList();
+        showList(filiterValue);
+    }
 }
 
-const addButton = document.querySelector('#addNewItem');
-addButton.addEventListener('click', addItemToList);
+function changeFiliter() {
+    let filiterClicked = event.currentTarget;
+    filiterClicked = String(filiterClicked.id);
 
-showList();
+    switch (filiterClicked) {
+        case "allCurrentButton":
+            filiterValue = "current";
+            break;
+
+        case "allCompletedButton":
+            filiterValue = "completed";
+            break;
+
+        case "allTasksButton":
+            filiterValue = "all";
+            break;
+    }
+
+    showList(filiterValue);
+
+}
+
+window.addEventListener("load", () => {
+    const addButton = document.querySelector('#addNewItem');
+    addButton.addEventListener('click', addItemToList);
+
+    const allButton = document.querySelector('#allTasksButton');
+    allButton.addEventListener('click', changeFiliter);
+
+    const currentButton = document.querySelector('#allCurrentButton');
+    currentButton.addEventListener('click', changeFiliter);
+
+    const completeButton = document.querySelector('#allCompletedButton');
+    completeButton.addEventListener('click', changeFiliter);
+
+    filiterValue = "all";
+
+    showList(filiterValue);
+});
